10a6b77fabfe39a48f2b490ffcbcfee4e30429bc
cwilliams0926 <coopwilliams03@hotmail.com>
2025-12-10 18:52:15 -0500

    Add Person class

---
+++ assignment 6/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_6
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+
+        public Person()
+        {
+
+        }
+    }
+}
