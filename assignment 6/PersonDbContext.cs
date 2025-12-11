f895443ec1e254864c80b5b5449ef40b41e91b3c
cwilliams0926 <coopwilliams03@hotmail.com>
2025-12-10 19:08:35 -0500

    Add PersonDbContext

---
+++ assignment 6/PersonDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace assignment_6
+{
+    public class PersonDbContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite("Data Source=people.db");
+    }
+}
